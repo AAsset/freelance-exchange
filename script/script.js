a116c2a460b56a7cd125600f55b22a2830950796
Asset <asset.aktay@gmail.com>
2019-12-18 16:59:24 +0600

    render table orders

--- script/script.js
+++ script/script.js
@@ -7,9 +7,26 @@ document.addEventListener('DOMContentLoaded', () => {
           blockFreelancer = document.querySelector('#block-freelancer'),
           blockChoice = document.querySelector('#block-choice'),
           btnExit = document.querySelector('#btn-exit'),
-          formCustomer = document.querySelector('#form-customer');
+          formCustomer = document.querySelector('#form-customer'),
+          ordersTable = document.querySelector('#orders');
 
     const orders = [];
+
+    const renderOrders = () => {
+
+        ordersTable.textContent = '';
+
+        orders.forEach((order, i) => {
+            ordersTable.innerHTML += `
+                <tr class="order" data-number-order="${i}">
+                    <td>${i + 1}</td>
+                    <td>${order.title}</td>
+                    <td class="${order.currency}"></td>
+                    <td>${order.deadline}</td>
+                </tr>`;
+        });
+    };
+
 
     customer.addEventListener('click', () => {
         blockChoice.style.display = 'none';
@@ -19,6 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     freelancer.addEventListener('click', () => {
         blockChoice.style.display = 'none';
+        renderOrders();
         blockFreelancer.style.display = 'block';
         btnExit.style.display = 'block';
     });
